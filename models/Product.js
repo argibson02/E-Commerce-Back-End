1a2069925028b42d150a4b156befaae5808053b7
Alex Gibson <argibson02@gmail.com>
2021-10-26 21:06:06 -0700

    Built out tag.js class. Added id to all classes.

--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,17 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
   },
   {
     sequelize,
@@ -25,41 +30,41 @@ module.exports = Product;
 // * `Product`
 
 //   * `id`
-  
+
 //     * Integer.
-  
+
 //     * Doesn't allow null values.
-  
+
 //     * Set as primary key.
-  
+
 //     * Uses auto increment.
 
 //   * `product_name`
-  
+
 //     * String.
-  
+
 //     * Doesn't allow null values.
 
 //   * `price`
-  
+
 //     * Decimal.
-  
+
 //     * Doesn't allow null values.
-  
+
 //     * Validates that the value is a decimal.
 
 //   * `stock`
-  
+
 //     * Integer.
-  
+
 //     * Doesn't allow null values.
-  
+
 //     * Set a default value of `10`.
-  
+
 //     * Validates that the value is numeric.
 
 //   * `category_id`
-  
+
 //     * Integer.
-  
+
 //     * References the `Category` model's `id`.
--- models/Tag.js
+++ models/Tag.js
@@ -2,11 +2,19 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
@@ -23,15 +31,15 @@ module.exports = Tag;
 // * `Tag`
 
 //   * `id`
-  
+
 //     * Integer.
-  
+
 //     * Doesn't allow null values.
-  
+
 //     * Set as primary key.
-  
+
 //     * Uses auto increment.
 
 //   * `tag_name`
-  
+
 //     * String.
