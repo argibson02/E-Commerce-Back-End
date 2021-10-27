352fddae33a4a12beb799a3c65e229d51a3ac65a
Alex Gibson <argibson02@gmail.com>
2021-10-27 16:33:59 -0700

    Updated seed products

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -2,31 +2,31 @@ const { Product } = require('../models');
 console.log(7);
 const productData = [
   {
-    product_name: 'Plain T-Shirt',
+    product_name: 'Salt Life Inline Woven Performance Fishing Shirt',
     price: 14.99,
     stock: 14,
     category_id: 1,
   },
   {
-    product_name: 'Running Sneakers',
+    product_name: 'New Balance 674',
     price: 90.0,
     stock: 25,
     category_id: 5,
   },
   {
-    product_name: 'Branded Baseball Hat',
+    product_name: 'Ducks Unlimited Baseball Hat',
     price: 22.99,
     stock: 12,
     category_id: 4,
   },
   {
-    product_name: 'Top 40 Music Compilation Vinyl Record',
+    product_name: 'Jimmy Buffett\'s Songs You Know by Heart Vinyl Record',
     price: 12.99,
     stock: 50,
     category_id: 3,
   },
   {
-    product_name: 'Cargo Shorts',
+    product_name: 'Big Rock Zip-Off Pants',
     price: 29.99,
     stock: 22,
     category_id: 2,
