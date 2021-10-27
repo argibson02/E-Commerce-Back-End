05d378dd56d0090054a90242506bf511bf6cccac
Alex Gibson <argibson02@gmail.com>
2021-10-26 21:20:04 -0700

    Built out Product.js class

--- models/Product.js
+++ models/Product.js
@@ -15,6 +15,32 @@ Product.init(
       primaryKey: true,
       autoIncrement: true
     },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Category',
+        key: 'id',
+      }
+    }
   },
   {
     sequelize,
@@ -26,45 +52,3 @@ Product.init(
 );
 
 module.exports = Product;
-
-// * `Product`
-
-//   * `id`
-
-//     * Integer.
-
-//     * Doesn't allow null values.
-
-//     * Set as primary key.
-
-//     * Uses auto increment.
-
-//   * `product_name`
-
-//     * String.
-
-//     * Doesn't allow null values.
-
-//   * `price`
-
-//     * Decimal.
-
-//     * Doesn't allow null values.
-
-//     * Validates that the value is a decimal.
-
-//   * `stock`
-
-//     * Integer.
-
-//     * Doesn't allow null values.
-
-//     * Set a default value of `10`.
-
-//     * Validates that the value is numeric.
-
-//   * `category_id`
-
-//     * Integer.
-
-//     * References the `Category` model's `id`.
